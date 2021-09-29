59943ac31b099bd9c593b85c2c330d54adbbad04
matthieugi <matthieugirard@outlook.com>
2021-09-29 12:46:05 +0000

    add heart

--- index.js
+++ index.js
@@ -29,5 +29,9 @@ app.post('/heart', (req, res) => {
   });
 });
 
+app.get('/heart', (req, res) => {
+  res.send('♥');
+})
+
 app.listen(port);
 console.log(`Server running on http://localhost:${port}`)
